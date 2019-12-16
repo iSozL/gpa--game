4cfec2576f1f298c413d67fbe96a2e752b9d1253
jello <2511841570@qq.com>
2019-12-16 18:06:25 +0800

    :lipstick: add index ui

--- gpa-game/src/index/index.tsx
+++ gpa-game/src/index/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import './index.css'
 const Index = () => {
-  return(<div>test</div>)
+  return(
+    <div className="index-container">
+      <div className="index-title">2019</div>
+      <div className="index-flag">生成的你的年度#TAG#</div>
+    </div>
+  )
 }
 export default Index;
