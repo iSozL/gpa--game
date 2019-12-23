c96c28a6250fe3807dade9041608054e55064ac5
jello <2511841570@qq.com>
2019-12-23 22:45:34 +0800

    :sparkles: 增加路由

--- gpa-game/src/index.js
+++ gpa-game/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import './index.css';
 import Index from './index/index.tsx';
+import Papers from './papers/papers.tsx'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+const Routers = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/papers" component={Papers}></Route>
+        <Route path="/" component={Index} exact></Route>
+      </Switch>
+    </Router>
+  )
+} 
+ReactDOM.render(<Routers />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ gpa-game/src/papers/papers.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './paper.css'
+const Papers = () => {
+  return(
+    <div></div>
+  )
+}
+export default Papers;
