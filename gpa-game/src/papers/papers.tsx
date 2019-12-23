90bf833823a8a221b60e0f01e16f6bebecee022a
jello <2511841570@qq.com>
2019-12-23 23:14:42 +0800

    :lipstick: 背景模糊

--- gpa-game/src/papers/papers.tsx
+++ gpa-game/src/papers/papers.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import './paper.css'
 const Papers = () => {
   return(
-    <div className="paper-container"></div>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <div className="paper-container"></div>
+      <div className="paper">
+
+      </div>
+    </div>
   )
 }
 export default Papers;
