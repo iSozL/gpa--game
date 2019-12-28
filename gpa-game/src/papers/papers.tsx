e16533469b6a09ba679443b949589176a141674d
jello <2511841570@qq.com>
2019-12-29 00:05:50 +0800

    :lipstick: 完成一部分样式

--- gpa-game/src/papers/papers.tsx
+++ gpa-game/src/papers/papers.tsx
@@ -1,10 +1,25 @@
-import * as React from 'react';
+import React from 'react';
 import './paper.css'
 const Papers = () => {
+  const papers = [
+    {
+      name: "缘起",
+      bgcolor: "rgba(40, 97, 167, .5)",
+      logo: require('../assets/imgs/nculogo.jpg')
+    }
+  ]
   return(
     <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
       <div className="paper-container"></div>
       <div className="paper">
+        {
+          papers.map((item, index) => (
+            <div key={index} className="paper-name">
+              <span style={{background: item.bgcolor}}>{item.name}</span>
+              <div><div></div></div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
