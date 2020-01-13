dd5acdfea642fa11b2c76594570940b86c873968
jello <2511841570@qq.com>
2020-01-13 22:29:48 +0800

    :sparkles: 随机人设

--- gpa-game/src/characters/index.jsx
+++ gpa-game/src/characters/index.jsx
@@ -2,10 +2,25 @@ import React from "react"
 import './index.css'
 import Loading from "../components/loading"
 const Characters = () => {
-  if(window.location.href.indexOf('#reloaded')==-1){ //判断是否有刷新标记
-    window.location.href=window.location.href+"#reloaded";//没有添加标记
+  if(window.location.href.indexOf('#reload')==-1){ //判断是否有刷新标记
+    window.location.href=window.location.href+"#reload";//没有添加标记
     window.location.reload();//刷新
   }
+  const opp = Math.round(Math.round(Math.random() * 30 + 10) / 10)
+  const imgs = [
+    {
+      img: require("../assets/imgs/rxjl.png")
+    },
+    {
+      img: require("../assets/imgs/zcm.png")
+    },
+    {
+      img: require("../assets/imgs/hhy.png")
+    },
+    {
+      img: require("../assets/imgs/mty.png")
+    },
+  ]
   window.onload = function() {
     console.log("finish loading")
     document.getElementById("char").style.visibility = "visible"
@@ -15,7 +30,7 @@ const Characters = () => {
     <div>
       <div id="char-loading"><Loading /></div>
       <div className="characters-container" id="char">
-        <img src={require("../assets/imgs/rxjl.png")} className="characters"></img>
+        <img src={imgs[opp - 1]} className="characters"></img>
       </div>
     </div>
   )
